0dc878ea1005398bac0e0dfc7f0c31d55227ad08
Todd Gladfelter <todd.gladfelter@Todds-Tiny-Mac.local>
2019-10-15 23:15:10 -0700

    added dom project touches

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,64 @@
-console.log("Up and running!");
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+var cards = [
+   {
+       rank: "queen",
+       suit: "hearts",
+       cardImage: "images/queen-of-hearts.png",
+   },
+   {
+      rank: "queen",
+      suit: "diamonds",
+      cardImage: "images/queen-of-diamonds.png",
+   },
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
-	alert("You found a match!");
+   {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+   },
+    {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+   },
+];
+
+var cardsInPlay = [];
+
+
+function checkForMatch(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
 } else {
-	alert("Sorry, try again.");
+  alert("Sorry, try again.");
+};
 }
+
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+ //console.log("User flipped" + cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].rank);
+ this.setAttribute('src', cards[cardId].cardImage);
+//console.log(cards[cardId].suit);
+ if (cardsInPlay.length === 2){
+    checkForMatch();
+    //cardsInPlay = [];
+
+ };
+};
+
+console.log(cardsInPlay);
+
+
+ function createBoard() {
+  const cardTable = document.getElementById('game-board');
+  for (var i = 0; i < cards.length; i++) {
+  var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    cardTable.appendChild(cardElement);
+
+  }
+};
+
+createBoard();
